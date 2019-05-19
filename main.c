d5545ec73b302f3d1b44b8ef9f3fd48f7b2fa553
ip815s10 <pukan05@yandex.ru>
2019-05-19 15:13:43 +0600

    Create Menu()

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <curses.h>
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,9 +7,29 @@ typedef unsigned int u_int;
 typedef enum Direction { up, down, right, left } Direction;
 u_short Field[4][4];
 u_short CurX, CurY;
+void Menu()
+{
+    printw("1.New game \n");
+    printw("2. Autors\n");
+    printw("3.Exit \n");
+}
 int main()
 {
+    initscr();
     int exit = 0;
     exit += 1;
-    return 0;
+    int c;
+    Menu();
+    while (1) {
+        c = getch();
+        if (c == 49) {
+        }
+        if (c == 50) {
+        }
+        if (c == 51) {
+            endwin();
+            return 0;
+        }
+    }
 }
+
