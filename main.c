ae8680cd85d9c1c4c9f78abcbac475a09379b7b4
ip815s10 <pukan05@yandex.ru>
2019-05-19 12:38:15 +0600

    Connecting libraries

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
-
+#include <curses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+typedef unsigned short u_short;
+typedef unsigned int u_int;
+typedef enum Direction { up, down, right, left } Direction;
+u_short Field[4][4];
+u_short CurX, CurY;
+int main()
+{
+    int exit = 0;
+    exit += 1;
+    return 0;
+}
